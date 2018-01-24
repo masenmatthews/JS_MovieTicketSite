58f4d42e19390c4a497d1a141937201378e57466
Masen Matthews and Ron Craig <masen@>
2018-01-23 16:42:14 -0800

    enhance js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -9,28 +9,29 @@ function Ticket (name, age, movie, time) {
 //   return this.street + ", " + this.city + ", " + this.state;
 // };
 
-function resetFields() {
-      $("input#name-input").val("");
-      $("input#age").val("");
-      $("input.movie").val("");
-      $("input.time").val("");
-
-}
+// function resetFields() {
+//       $("input#name-input").val("");
+//       $("input#age").val("");
+//       $("input.movie").val("");
+//       $("input.time").val("");
+//
+// }
 
 
 
 $(document).ready(function() {
 
 
-  $("form#main-form").submit(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
     var yourName = $("#name-input").val();
-      console.log("#name-input");
-    var yourAge = $("#age").val();
-    var yourMovie = $("input:radio[name=movie]:checked").val();
-    var yourTime = $("input:radio[name=time]:checked").val();
+      debugger
+      console.log(yourName);
+    // var yourAge = $("input#age").val();
+    // var yourMovie = $("input:radio[name=movie]:checked").val();
+    // var yourTime = $("input:radio[name=time]:checked").val();
 
-    var newPatron = new Movie(youName, yourAge, yourMovie, yourTime);
-    alert("newPatron: " + newPatron);
+    // var newPatron = new Movie(yourName, yourAge, yourMovie, yourTime);
+
   });
 });
